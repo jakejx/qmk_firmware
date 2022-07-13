9f08d622ceedc43da027140ae824a39777edde5b
Jake Ng <njunxuan@gmail.com>
2022-07-13 17:13:20 +0800

    Add custom sofle keymap

---
+++ keyboards/sofle/keymaps/jakejx/config.h
@@ -0,0 +1,20 @@
+#pragma once
+
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
+
+#ifdef TAPPING_TERM
+#undef TAPPING_TERM
+#define TAPPING_TERM 160
+#endif
+
+#define TAPPING_TERM_PER_KEY
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// This prevents accidental repeats of the tap-hold keys when typing quickly.
+#define TAPPING_FORCE_HOLD_PER_KEY
+
+// Disable PERMISSIVE_HOLD
+#undef PERMISSIVE_HOLD
---
+++ keyboards/sofle/keymaps/jakejx/rules.mk
@@ -0,0 +1,10 @@
+.PHONY: %
+
+%:
+	keymapviz -k sofle -t fancy -r keyboards/sofle/keymaps/jakejx/keymap.c -c keyboards/sofle/keymaps/jakejx/keymapviz.toml
+
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306
+ENCODER_ENABLE = yes
+CONSOLE_ENABLE = no
+EXTRAKEY_ENABLE = yes
